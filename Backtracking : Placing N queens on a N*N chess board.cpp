bb6fe68ce6ffdb42b3996c85194a938bc75dec0d
Sandeep singh <62880005+sandeepKashnyal@users.noreply.github.com>
2020-10-01 20:44:47 +0530

    Update Backtracking : Placing N queens on a N*N chess board.cpp

--- Backtracking : Placing N queens on a N*N chess board.cpp
+++ Backtracking : Placing N queens on a N*N chess board.cpp
@@ -4,10 +4,10 @@ int ctr = 0;
 
 void backtrack(int ptr,int n, int pos[], int dg1[], int dg2[], int ch[]){
 	if (ptr == n){
-		cout<<"Permutation "<<++ctr<<" : \n";
+		cout << "Permutation " << ++ctr << " : \n";
 		for (int i = 0; i<n; i++){
 			for (int j = 0; j<n; j++){
-				cout<<((pos[i] == j)? "Q " : "+ ");
+				cout << ((pos[i] == j)? "Q " : "+ ");
 			}
 			cout<<"\n";
 		}
